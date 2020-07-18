4845d6baeb261aaa3121f9a8a1614e69d36d6b85
Stella Gurin <59928550+stellagurin@users.noreply.github.com>
2020-07-18 17:23:59 -0400

    Added files

---
+++ switch.js
@@ -0,0 +1,47 @@
+$('#default').click(function() {
+	$('link[rel="stylesheet"]').attr({
+		href: 'default.css'
+	});
+});
+
+$('#black').click(function() {
+	$('link[rel="stylesheet"]').attr({
+		href: 'black.css'
+	});
+});
+	
+$('#blue').click(function() {
+	$('link[rel="stylesheet"]').attr({
+		href: 'blue.css'
+	});
+});
+	
+$('#brown').click(function() {
+	$('link[rel="stylesheet"]').attr({
+		href: 'brown.css'
+	});
+});
+	
+$('#gold').click(function() {
+	$('link[rel="stylesheet"]').attr({
+		href: 'gold.css'
+	});
+});
+	
+$('#navy').click(function() {
+	$('link[rel="stylesheet"]').attr({
+		href: 'navy.css'
+	});
+});
+	
+$('#pink').click(function() {
+	$('link[rel="stylesheet"]').attr({
+		href: 'pink.css'
+	});
+});
+	
+$('#red').click(function() {
+	$('link[rel="stylesheet"]').attr({
+		href: 'red.css'
+	});
+});
